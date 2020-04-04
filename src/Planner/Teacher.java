d76438aa0fc69f0f89e638ce6d3e57cdc6c3141f
Attednev <damian.gasser@gmail.com>
2020-04-04 16:23:14 +0200

    Started with Teacher class

--- src/Planner/Teacher.java
+++ src/Planner/Teacher.java
@@ -1,4 +1,17 @@
 package Planner;
 
-public class Teacher {
+import Draggable.Draggable;
+import javafx.scene.Node;
+
+public class Teacher extends Authority {
+    public Teacher(String name) {
+        super(name);
+    }
+
+    @Override
+    protected Node getCopyOfInstance() {
+        Authority copy = new Authority(this.name);
+        copy.setTimeTable(this.timeTable);
+        return copy;
+    }
 }
