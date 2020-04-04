56db7b761553f4b66990c7027ab11d62595702b3
Attednev <damian.gasser@gmail.com>
2020-04-04 16:24:19 +0200

    Extendet controller for testing the draggable class

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -25,5 +25,30 @@ public class Controller {
 
     @FXML
     private void initialize() {
+        this.teachers.addListener((ListChangeListener<Teacher>) c -> this.box1.getChildren().setAll(this.teachers));
+        this.authorities.addListener((ListChangeListener<Authority>) c -> this.box2.getChildren().setAll(this.authorities));
+        // this.councils.addListener((ListChangeListener<Council>) c -> this.box1.getChildren().setAll(this.councils));
+
+        // Code for debugging
+        this.box1.setStyle("-fx-border-color: black; -fx-background-color: red");
+        this.box1.setPrefSize(250, 250);
+        this.box2.setStyle("-fx-border-color: black; -fx-background-color: green");
+        this.box2.setPrefSize(250, 250);
+        this.box3.setStyle("-fx-border-color: black; -fx-background-color: yellow");
+        this.box3.setPrefSize(250, 500);
+
+        Authority authority1 = new Authority("Authority 1");
+        authority1.makeDraggable(this.root, this.box3);
+        Authority authority2 = new Authority("Authority 2");
+        authority2.makeDraggable(this.root, this.box3);
+        this.authorities.add(authority1);
+        this.authorities.add(authority2);
+
+        Teacher teacher1 = new Teacher("Teacher 1");
+        teacher1.makeDraggable(this.root, this.box3);
+        Teacher teacher2 = new Teacher("Teacher 2");
+        teacher2.makeDraggable(this.root, this.box3);
+        this.teachers.add(teacher1);
+        this.teachers.add(teacher2);
     }
 }
