d309763631c1e204a1f95ad17a145dde16d15157
Attednev <damian.gasser@gmail.com>
2020-04-15 14:51:55 +0200

    Extended the controller and implemented feature to add teacher, council and authority

--- src/Main/MainSceneController.java
+++ src/Main/MainSceneController.java
@@ -1,4 +1,82 @@
 package Main;
 
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
+import java.util.Stack;
+
 public class MainSceneController {
+
+    @FXML
+    private VBox councilsContainer;
+    @FXML
+    private VBox authorityContainer;
+    @FXML
+    private VBox teachersContainer;
+
+    @FXML
+    private void initialize() {
+        // Adding buttons for allowing the user to add councils, teachers and authorities
+        StackPane addCouncilButton = createCircularButton();
+        StackPane addTeacherButton = createCircularButton();
+        StackPane addAuthorityButton = createCircularButton();
+        addCouncilButton.setOnMouseClicked(e -> this.addCouncil());
+        addTeacherButton.setOnMouseClicked(e -> this.addTeacher());
+        addAuthorityButton.setOnMouseClicked(e -> this.addAuthority());
+        this.councilsContainer.getChildren().add(addCouncilButton);
+        this.teachersContainer.getChildren().add(addTeacherButton);
+        this.authorityContainer.getChildren().add(addAuthorityButton);
+    }
+
+    private void addCouncil() {
+        System.out.println("Adding Council");
+    }
+
+    private void addAuthority() {
+        System.out.println("Adding Authority");
+    }
+
+    private void addTeacher() {
+        System.out.println("Adding Teacher");
+    }
+
+    // Helper function for creating a circular button
+    public static StackPane createCircularButton() {
+        StackPane pane = new StackPane();
+        Circle circle = new Circle(20);
+        Line topDown = new Line(20, 13, 20, 26);
+        Line leftRight = new Line(13, 20, 26, 20);
+        EventHandler<? super MouseEvent> entered = (EventHandler<MouseEvent>) event -> {
+            circle.setStyle("-fx-stroke-width: 2px; -fx-stroke: black; -fx-fill: #6EC30C");
+            pane.setScaleX(1.1);
+            pane.setScaleY(1.1);
+        };
+        EventHandler<? super MouseEvent> exited = (EventHandler<MouseEvent>) event -> {
+            circle.setStyle("-fx-stroke-width: 2px; -fx-stroke: black; -fx-fill: lime");
+            pane.setScaleX(1);
+            pane.setScaleY(1);
+        };
+        circle.setOnMouseEntered(entered);
+        circle.setOnMouseExited(exited);
+        circle.setStyle("-fx-stroke-width: 2px; -fx-stroke: black; -fx-fill: lime");
+        topDown.setOnMouseEntered(entered);
+        topDown.setOnMouseExited(exited);
+        topDown.setStyle("-fx-stroke-width: 2px; -fx-stroke: black");
+        leftRight.setOnMouseEntered(entered);
+        leftRight.setOnMouseExited(exited);
+        leftRight.setStyle("-fx-stroke-width: 2px; -fx-stroke: black");
+        pane.setPrefSize(40, 40);
+        pane.getChildren().addAll(circle, topDown, leftRight);
+        return pane;
+    }
+
+    // Function for starting the calculation process
+    @FXML
+    private void calculate(MouseEvent mouseEvent) {
+    }
 }
