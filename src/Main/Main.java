6b184f985c30d1c67bdefbb7abbd051cf7a96587
Attednev <damian.gasser@gmail.com>
2020-04-27 15:28:44 +0200

    Changed window header

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("Scene.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Krsp - Klassenratssitzungsplaner");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
