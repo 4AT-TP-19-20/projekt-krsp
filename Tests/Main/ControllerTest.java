520cbebfb98739b407907da77d55d6ec14bf8266
Manuel <manupl2002@gmail.com>
2020-05-11 17:10:26 +0200

    add ControllerTest

---
+++ Tests/Main/ControllerTest.java
@@ -0,0 +1,12 @@
+package Main;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+    @BeforeAll
+    static void beforeAll() {
+        Main.main(new String[] {});
+    }
+}
