f50c784ed01ab3bae2626953d99dadf735946763
Manuel <manupl2002@gmail.com>
2020-05-11 17:21:35 +0200

    add Test for overlaps in ControllerTest

--- Tests/Main/ControllerTest.java
+++ Tests/Main/ControllerTest.java
@@ -1,6 +1,9 @@
 package Main;
 
+import Planner.Interval;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +11,16 @@ class ControllerTest {
     @BeforeAll
     static void beforeAll() {
         Main.main(new String[] {});
+    }
+
+    @Test
+    void overlaps() {
+        Interval i = new Interval(2,4);
+        Interval j = new Interval(3,7);
+        double min = Math.min(Double.parseDouble(i.getEndValue()), Double.parseDouble(j.getEndValue()));
+        double max = Math.max(Double.parseDouble(i.getStartValue()), Double.parseDouble(j.getStartValue()));
+        Assertions.assertEquals(4, min);
+        Assertions.assertEquals(3, max);
+        Assertions.assertTrue(min <= max);
     }
 }
